61a13bf015ce7ccfcedda4e805cc9e96e5d3a201
heroichunter19 <theprofessionalman.nayem@gmail.com>
2026-06-03 21:00:02 +0600

    test commit 2

--- sourcefld/sceens/home.py
+++ sourcefld/sceens/home.py
@@ -1,12 +1,20 @@
 import streamlit as sl
 from sourcefld.compofile.header import home_header
-
+from sourcefld.uifolder.style_layout import style_layout , style_layout_main , style_layout_other
 
 def  home_screenload():
+
+    
     
 
 
     home_header() 
+    style_layout_main()
+    
+
+    style_layout()
+
+
     col1, col2 = sl.columns(2)
 
     with col1:
